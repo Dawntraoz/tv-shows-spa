21d8f5403311b3c8605e92da5f4fbc0d30bb8f55
albasilventefuentes <alba@square1.io>
2020-05-23 22:36:45 +0200

    Create elements on Listing layout

---
+++ src/components/ShowsListItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card color="transparent" flat width="200">
+    <router-link :to="{ name: 'show', params: { id: item.id } }">
+      <v-card elevation="6">
+        <v-img
+          :alt="item.name"
+          :src="item.image.medium"
+          width="175"
+          height="275"
+        />
+      </v-card>
+    </router-link>
+    <h3 class="pt-2">{{ item.name }}</h3>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "ShowsListItem",
+  props: {
+    item: Object
+  }
+};
+</script>
---
+++ src/components/ShowsListTitle.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-col cols="12">
+    <h2>{{ title }}</h2>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: "ShowsListTitle",
+  props: {
+    title: String
+  }
+};
+</script>
