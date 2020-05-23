d2293d5bf6b17725a66a4b55e612cb513530213d
albasilventefuentes <alba@square1.io>
2020-05-23 22:37:26 +0200

    Show Listing initial slider

--- src/views/ShowsListing.vue
+++ src/views/ShowsListing.vue
@@ -1,29 +1,50 @@
 <template>
-  <div class="shows-listing">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <div v-for="show in shows" :key="'Show ' + show.id">
-      <router-link :to="{ name: 'show', params: { id: show.id } }">
-        {{ show.name }}
-      </router-link>
-    </div>
-  </div>
+  <v-container>
+    <v-row v-for="genre in genres" :key="genre">
+      <ShowsListTitle :title="genre" />
+      <v-slide-group multiple>
+        <v-slide-item
+          v-for="show in showsByGenre(genre)"
+          :key="'Show ' + show.id"
+          class="px-4"
+        >
+          <ShowsListItem :item="show" />
+        </v-slide-item>
+      </v-slide-group>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
 import { getAllShows } from "@/api/shows.api";
-import HelloWorld from "@/components/HelloWorld.vue";
+import ShowsListTitle from "@/components/ShowsListTitle.vue";
+import ShowsListItem from "@/components/ShowsListItem.vue";
 
 export default {
   name: "ShowsListing",
-  components: {
-    HelloWorld
-  },
   data: () => ({
+    genres: [],
     shows: []
   }),
+  components: {
+    ShowsListTitle,
+    ShowsListItem
+  },
   mounted() {
-    getAllShows().then(res => (this.shows = res.data));
+    getAllShows().then(res => {
+      res.data.map(show => {
+        show.genres.map(genre => {
+          if (!this.genres.some(g => g === genre)) this.genres.push(genre);
+        });
+      });
+      this.shows = res.data;
+    });
+  },
+  methods: {
+    showsByGenre: function(genre) {
+      return this.shows.filter(show => show.genres.some(g => g === genre));
+    }
   }
 };
 </script>
