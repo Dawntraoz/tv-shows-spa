2b0e1a879a7a7896c0c51a8a19df21d35681a068
albasilventefuentes <alba@square1.io>
2020-05-24 21:38:14 +0200

    Resolve conflicts ShowDetails test and create TheSearch one

--- tests/unit/TheSearch.spec.js
+++ tests/unit/TheSearch.spec.js
@@ -4,11 +4,11 @@ import Vuetify from "vuetify";
 
 // Store
 import Vuex from "vuex";
-import Shows from "@/store/modules/shows";
+import Search from "@/store/modules/search";
 import { cloneDeep } from "lodash";
 
 // Components
-import ShowDetail from "@/views/ShowDetail.vue";
+import TheSearch from "@/components/TheSearch.vue";
 
 // Utilities
 import { createLocalVue, shallowMount } from "@vue/test-utils";
@@ -18,14 +18,14 @@ const localVue = createLocalVue();
 localVue.use(Vuetify);
 localVue.use(Vuex);
 
-describe("ShowDetail.vue", () => {
+describe("TheSearch.vue", () => {
   let store;
   let wrapper;
 
   beforeEach(() => {
     store = new Vuex.Store({
       modules: {
-        Shows: cloneDeep(Shows)
+        Search: cloneDeep(Search)
       }
     });
 
@@ -39,7 +39,7 @@ describe("ShowDetail.vue", () => {
   });
 
   const mountFunction = options => {
-    return shallowMount(ShowDetail, {
+    return shallowMount(TheSearch, {
       localVue,
       store,
       ...options
@@ -47,6 +47,6 @@ describe("ShowDetail.vue", () => {
   };
 
   it("should have a component name", () => {
-    expect(wrapper.name()).toMatch("ShowDetail");
+    expect(wrapper.name()).toMatch("TheSearch");
   });
 });
