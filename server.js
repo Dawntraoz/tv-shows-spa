ff6c6a2d76bc258e41c887fd33165e487632fd9d
albasilventefuentes <alba@square1.io>
2020-05-25 15:39:59 +0200

    Production mode with express app

---
+++ server.js
@@ -0,0 +1,11 @@
+const history = require('connect-history-api-fallback')
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+app = express()
+app.use(history())
+
+app.use(serveStatic(path.join(__dirname, 'dist')))
+const port = process.env.PORT || 80
+app.listen(port)
