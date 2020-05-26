55c7646140e7335c06f2a789caedada4086d5ba7
albasilventefuentes <alba@square1.io>
2020-05-26 23:02:54 +0200

    Test before route update with Store mock

--- tests/unit/ShowDetail.spec.js
+++ tests/unit/ShowDetail.spec.js
@@ -2,7 +2,10 @@
 import ShowDetail from '@/views/ShowDetail.vue'
 
 // Custom store
-import store from '@/store'
+import Vuex from 'vuex'
+
+// Custom router
+import router from '@/router'
 
 // Utilities
 import { appInit } from './app-init'
@@ -11,11 +14,32 @@ import { createLocalVue, shallowMount } from '@vue/test-utils'
 const localVue = appInit(createLocalVue())
 
 describe('ShowDetail.vue', () => {
+  let getters = {
+    getShowInfo: jest.fn().mockReturnValue([{}]),
+    getShowImages: jest.fn().mockReturnValue([{}]),
+  }
+  let actions = {
+    fetchShow: jest.fn(),
+  }
+  let Shows = {
+    namespaced: true,
+    getters,
+    actions,
+  }
+  let store = new Vuex.Store({
+    modules: {
+      Shows: Shows,
+    },
+  })
   let wrapper
 
   const mountFunction = options => {
     return shallowMount(ShowDetail, {
       localVue,
+      propsData: {
+        id: 1,
+      },
+      router,
       store,
       ...options,
     })
@@ -34,5 +58,29 @@ describe('ShowDetail.vue', () => {
   it('should be a Vue instance and be called ShowDetail', async () => {
     expect(wrapper.isVueInstance()).toBeTruthy()
     expect(wrapper.name()).toMatch('ShowDetail')
+  })
+
+  describe('Computed properties', () => {
+    it('should return a void bg image', () => {
+      expect(wrapper.vm.backgroundImage).toBe('')
+    })
+  })
+
+  it('should call the before route update hook', async () => {
+    const beforeRouteUpdate = wrapper.vm.$options.beforeRouteUpdate[0]
+    const next = jest.fn()
+
+    await beforeRouteUpdate.call(
+      wrapper.vm,
+      {
+        params: {
+          id: 1,
+        },
+      },
+      {},
+      next,
+    )
+
+    expect(next).toHaveBeenCalled()
   })
 })
