28033872c4e930f63b3d924b57717e84a5627272
albasilventefuentes <alba@square1.io>
2020-06-02 10:58:36 +0200

    http redirect as per requirement PWA

--- server.js
+++ server.js
@@ -7,5 +7,11 @@ app = express()
 app.use(history())
 
 app.use(serveStatic(path.join(__dirname, 'dist')))
+
+// set up a route to redirect http to https
+app.get('*', function(req, res) {
+  res.redirect('https://' + req.headers.host + req.url)
+})
+
 const port = process.env.PORT || 80
 app.listen(port)
